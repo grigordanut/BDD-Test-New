a0901451d58c4df6b5b4bb472bfd7df087d61e69
Grigor Danut <grigor@seamlesscare.ie>
2023-09-18 18:05:36 +0100

    first commit Aded England Feature File and Steps

---
+++ HomeAppliancesCostNew/Features/EnglandCustomer.feature
@@ -0,0 +1,16 @@
+﻿Feature: EnglandCustomer
+
+A short summary of the feature
+
+Scenario Outline: As a resident from England estimate of how much electrical appliances cost to run
+	Given I am a resident from England
+	When I add the list appliances "<apliancename>" average use <hours>, <minutes>, "<frequency>", <rate>, and its average usage and the national average rate
+	Then I should get the results table with daily, weekly, monthly, and yearly cost
+
+	Examples: 
+
+	| apliancename     | hours | minutes | frequency | rate |
+	| Electric blanket | 3     | 30      | day       | 30   |
+	| Immersion heater | 8     | 20      | day       | 30   |
+	| Fan heater       | 45    | 30      | week      | 30   |
+	| Broadband router | 60    | 20      | week      | 30   |
---
+++ HomeAppliancesCostNew/StepDefinitions/EnglandCustomerStepDefinitions.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using TechTalk.SpecFlow;
+
+namespace HomeAppliancesCostNew.StepDefinitions
+{
+    [Binding]
+    public class EnglandCustomerStepDefinitions
+    {
+        IWebDriver driver;        
+        [Given(@"I am a resident from England")]
+        public void GivenIAmAResidentFromEngland()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.citizensadvice.org.uk/");
+            driver.FindElement(By.XPath("//*[@id=\"home-extent-popup\"]/div/div/a[1]")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("//*[@id=\"main-nav\"]/ul/li[4]/a")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"main\"]/div[2]/div/div[2]/ul/li/a")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"main\"]/div[2]/div/div[2]/ul/li[6]/a")).Click();
+        }
+
+        [When(@"I add the list appliances ""([^""]*)"" average use (.*), (.*), ""([^""]*)"", (.*), and its average usage and the national average rate")]
+        public void WhenIAddTheListAppliancesAverageUseAndItsAverageUsageAndTheNationalAverageRate(string appliance, int hours, int minutes, string frequency, int rate)
+        {
+            SelectElement selecteAppliance = new(driver.FindElement(By.XPath("//*[@id=\"appliance\"]")));
+            selecteAppliance.SelectByText(appliance);
+            driver.FindElement(By.XPath("//*[@id=\"hours\"]")).SendKeys("" + hours);
+            driver.FindElement(By.XPath("//*[@id=\"mins\"]")).SendKeys("" + minutes);
+            SelectElement selectedFrequency = new SelectElement(driver.FindElement(By.XPath("//*[@id=\"frequency\"]")));
+            selectedFrequency.SelectByValue(frequency);
+            driver.FindElement(By.XPath("//*[@id=\"kwhcost\"]")).SendKeys("" + rate);
+            driver.FindElement(By.XPath("//*[@id=\"submit\"]")).Click();
+        }
+
+        [Then(@"I should get the results table with daily, weekly, monthly, and yearly cost")]
+        public void ThenIShouldGetTheResultsTableWithDailyWeeklyMonthlyAndYearlyCost()
+        {
+            string actual = driver.FindElement(By.XPath("//*[@id=\"RootPlaceHolder_RootPlaceHolder_SubHeading\"]/span")).Text;
+            Console.WriteLine(actual);
+            String expected = "This advice applies to England";
+            Assert.AreEqual(actual, expected);
+            driver.FindElement(By.XPath("//*[@id=\"appliance_running\"]")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath("//*[@id=\"reset\"]")).Click();
+            driver.SwitchTo().Alert().Accept();
+            driver.Quit();
+        }
+    }
+}
