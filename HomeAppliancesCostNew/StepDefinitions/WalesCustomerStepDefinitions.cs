2f0a9e2783b7b978d3eac6508a04fa2697d7bd0b
Grigor Danut <grigor@seamlesscare.ie>
2023-09-19 08:01:44 +0100

    Added code to open the Citizen Advice website

--- HomeAppliancesCostNew/StepDefinitions/WalesCustomerStepDefinitions.cs
+++ HomeAppliancesCostNew/StepDefinitions/WalesCustomerStepDefinitions.cs
@@ -1,3 +1,5 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using TechTalk.SpecFlow;
 
@@ -12,6 +14,12 @@ namespace HomeAppliancesCostNew.StepDefinitions
         {
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.citizensadvice.org.uk/");
+            driver.FindElement(By.XPath("//*[@id=\"home-extent-popup\"]/div/div/a[4]")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("//*[@id=\"main-nav\"]/ul/li[4]/a")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"main\"]/div[2]/div/div[2]/ul/li/a")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"main\"]/div[2]/div/div[2]/ul/li[6]/a")).Click();
         }
 
         [When(@"I add the list appliance ""([^""]*)"" average use (.*), (.*), ""([^""]*)"", (.*), and its average usage and the national average rates")]
